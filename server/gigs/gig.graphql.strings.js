8706ee6e2302c6e83b37da9324dbd162a4c98236
raduchiriac <contact@raduchiriac.me>
2019-07-17 12:33:43 +0200

    Add better fake data

--- server/gigs/gig.graphql.strings.js
+++ server/gigs/gig.graphql.strings.js
@@ -2,36 +2,53 @@ const faker = require('faker');
 var ObjectID = require('mongodb').ObjectID;
 
 const randomIntFromInterval = (min, max) => Math.random() * (max - min) + min;
-
-const GIG_CREATE_FAKE = () => `
-  mutation {
-    createGig(input: {
-      _userId: "${new ObjectID()}",
-      _providerName: "${faker.name.firstName()} ${faker.name.lastName()}",
-      _rating: ${Math.random() * 4 + 1},
-      title: "${faker.company.catchPhrase()}",
-      images: [
-        "${faker.image.business()}/${Math.ceil(Math.random() * 20)}",
-        "${faker.image.people()}/${Math.ceil(Math.random() * 20)}",
-        "${faker.image.technics()}/${Math.ceil(Math.random() * 20)}}"
-      ],
-      description: "${faker.lorem.paragraph()}",
-      location: {
-        type: "Point",
-        address: "${faker.address.streetAddress()}, ${faker.address.country()}",
-        coordinates: [${randomIntFromInterval(50.75, 50.91)}, ${randomIntFromInterval(4.28, 4.45)}]
-      },
-      status: "valid",
-      tags: [${faker.lorem
-        .words()
-        .split(' ')
-        .map(w => `"${w}"`)
-        .join(', ')}],
-      price: ${Math.ceil(Math.random() * 5 + 1) * 10}
-    }) {
-      _id
-    }
+const randomColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+};
+const GIG_CREATE_FAKE = () => {
+  const userID = new ObjectID();
+  const title = faker.company.catchPhrase();
+  return `
+    mutation {
+      createGig(input: {
+        _userId: "${userID}",
+        _providerName: "${faker.name.firstName()} ${faker.name.lastName()}",
+        _rating: ${Math.random() * 4 + 1},
+        title: "${title}",
+        images: [
+          "https://i.pravatar.cc/256?u=${userID}",
+          "https://via.placeholder.com/256x256/${randomColor()}/${randomColor()}?text=${title
+    .match(/\b(\w)/g)
+    .join('')
+    .toUpperCase()}",
+          "https://via.placeholder.com/256x256/${randomColor()}/${randomColor()}?text=${title
+    .match(/\b(\w)/g)
+    .join('')
+    .toUpperCase()}"
+        ],
+        description: "${faker.lorem.paragraph()}",
+        location: {
+          type: "Point",
+          address: "${faker.address.streetAddress()}, ${faker.address.country()}",
+          coordinates: [${randomIntFromInterval(50.75, 50.91)}, ${randomIntFromInterval(4.28, 4.45)}]
+        },
+        status: "valid",
+        tags: [${faker.lorem
+          .words()
+          .split(' ')
+          .map(w => `"${w}"`)
+          .join(', ')}],
+        price: ${Math.ceil(Math.random() * 5 + 1) * 10}
+      }) {
+        _id
+      }
+    }
 `;
+};
 
 module.exports = { GIG_CREATE_FAKE };

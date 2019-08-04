72ab8e9e2cbfb10e0c6e7adc52229456e31db273
Radu Chiriac <contact@raduchiriac.me>
2019-08-04 19:57:52 +0200

    Reset Bruxelles center

--- client/App.js
+++ client/App.js
@@ -3,7 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './pages/Header';
+import Header from './Header';
 import HomePage from './pages/HomePage';
 import GigPage from './pages/GigPage';
 import UserPage from './pages/UserPage';
@@ -19,7 +19,7 @@ function App() {
       <MuiThemeProvider theme={DefaultTheme}>
         <CssBaseline />
         <Header />
-        <Container maxWidth="lg">
+        <Container maxWidth="lg" style={{ marginTop: '80px' }}>
           <Switch>
             <Route path="/" component={HomePage} exact={true} />
             <Route path="/service/:id" component={GigPage} />

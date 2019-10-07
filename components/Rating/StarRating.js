8ff0d3fb3a0af74c5338345779601520d15d34db
raduchiriac <contact@raduchiriac.me>
2019-10-08 00:20:01 +0200

    Improve StarRating component

--- components/Rating/StarRating.js
+++ components/Rating/StarRating.js
@@ -1,28 +1,40 @@
 import React, { Fragment } from 'react';
 import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 
 // https://material-ui.com/components/rating/#rating
 
 const StarRating = props => {
-  const { title, stars, total, size, disabled, readOnly, precision } = props;
-  const [value, setValue] = React.useState(stars);
+  const { title, score, maxStars, size, disabled, readOnly, precision, onChange, color, hoverColor, icon } = props;
+  const [value, setValue] = React.useState(parseInt(score, 10));
+
+  const StyledStarRating = withStyles({
+    iconFilled: {
+      color,
+    },
+    iconHover: {
+      color: hoverColor,
+    },
+  })(Rating);
 
   return (
     <Fragment>
-      <Box component="fieldset" mb={3} borderColor="transparent">
+      <Box component="fieldset">
         <Typography component="legend">{title}</Typography>
-        <Rating
-          name="simple-controlled"
+        <StyledStarRating
+          name="star-rating"
           value={value}
           size={size}
           disabled={disabled == 'true'}
           readOnly={readOnly == 'true'}
           precision={precision}
-          max={total}
+          max={maxStars}
+          icon={icon}
           onChange={(event, newValue) => {
             setValue(newValue);
+            onChange && onChange(newValue);
           }}
         />
       </Box>
--- components/_stories/rating.story.js
+++ components/_stories/rating.story.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import StarRating from '../Rating/StarRating';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 export default { title: 'Star Rating' };
 
-export const simpleWithDefaults = () => <StarRating />;
+export const justTheDefaults = () => <StarRating />;
 
-export const includingAllProps = () => (
+export const customColorsAndIcon = () => (
   <StarRating
-    title="I am a rating component"
-    stars="1.823442"
+    title="Do you like makeup?"
+    score="4.3333"
+    color="red"
+    hoverColor="#00FF11"
+    icon={<FavoriteIcon fontSize="inherit" />}
+  />
+);
+
+export const includingAllOtherProps = () => (
+  <StarRating
+    title="Hi, how much do you like me?"
+    score="1.823442"
     size="large"
     disabled="false"
     readOnly="false"
-    total={7}
+    maxStars={10}
+    onChange={value => console.log(`setting: ${value}`)}
     precision={0.5}
   />
 );
